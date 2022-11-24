30abb1c730fc88bb45f76df3b287e7c9e7d47dce
Pasan <pasankg@gmail.com>
2022-11-24 21:42:19 +0100

    Creating a clean App

--- kick-off/src/App.js
+++ kick-off/src/App.js
@@ -1,36 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-// App is a component.
-// This is in XML. We need to follow XML rules.
-// Rule 1 - Every element has to be closed using a closing tag or a self closing tag.
-// Rule 2 - Attribute values that are strings must be in double quotes. i.e.className="App"
-// Rule 3 - The curly braces{} represents js markup.
-// Rule 4 - If it looks like a HTML element, it has to be lowecase. 
-// Rule 5 - Keep in mind JS is case sensitive.
-// Rule 6 - Use camelcase for js keywords. i.e. className, htmlFor, firstName
-// Rule 7 - Return statement for this App function can ONLY return one element.
 function App() {
-  return (
-    // Start of a XML fragment.
-    <div className="App" htmlFor="" firstName="">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    // End of XML fragment.
-  );
+ return (
+  <div>Hello World !</div>
+ );
 }
+
+/**
+  This is JS module.
+  Everything inside this file is private by default.
+  To make it accessible we need to export it.
+
+  `default` is used to export one thing only.
+  if default is not used it means multiple items are exported.
+  i.e.  export {a,b,c} App
+ */
 
 export default App;
--- kick-off/src/index.js
+++ kick-off/src/index.js
@@ -1,21 +1,10 @@
-// Importing modules.
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// This is App.js , we don't need to give the .js extension.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// StrictMode is for development aid as it spots legacy code.
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+  <App />
+ </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
