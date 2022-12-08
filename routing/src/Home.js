058a8e71a38b5b27c9c09d8bfdeebb1a99457648
Pasan <pasankg@gmail.com>
2022-12-08 10:46:26 +0100

    React routing - initial commit

---
+++ routing/src/Home.js
@@ -0,0 +1,14 @@
+
+import Navbar from './Navbar'
+export default function Home(props) {
+ return (
+  <>
+   <Navbar />
+   <div className="container">
+    <div className='row'>
+     <h5>Home Page</h5>
+    </div>
+   </div>
+  </>
+ );
+}
---
+++ routing/src/ReactNews.js
@@ -0,0 +1,12 @@
+
+import Navbar from './Navbar'
+export default function ReactNews(props) {
+ return (
+  <>
+   <Navbar />
+   <div className="container">
+    <h5>React News Page</h5>
+   </div>
+  </>
+ );
+}
